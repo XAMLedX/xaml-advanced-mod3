86e5630b4fcf3110aa7883159815847ab277944f
Sidney Andrews <sidney@seesharprun.net>
2015-11-19 09:10:19 -0500

    Project Files

---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+        }
+
+        public void Detach()
+        {
+        }
+    }
+}
